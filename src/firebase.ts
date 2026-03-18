c3777c8a6c14355ce440509c7a6bbe7ab88e91eb
antonio-casaclick <casaclick.contacto@gmail.com>
2026-03-17 18:28:35 -0600

    Create firebase.ts

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFhxDwJAwCrUy0jU0Z2xUVQFxNMlTNK8",
+  authDomain: "casaclick-66471.firebaseapp.com",
+  projectId: "casaclick-66471",
+  storageBucket: "casaclick-66471.firebasestorage.app",
+  messagingSenderId: "813093034668",
+  appId: "1:813093034668:web:461e21876ab4c188215d09"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
